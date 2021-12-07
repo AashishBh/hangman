28da322eaf412ae360d67777934be58a709eb1e2
AashishBh <aaceesh.bhandary@gmail.com>
2021-12-07 23:27:31 +0545

    shortened random word choosing code

--- hangman.py
+++ hangman.py
@@ -1,5 +1,5 @@
 import random
 
 word_list = ['horse', 'door', 'song', 'trip', 'backbone', 'bomb', 'round', 'treasure', 'garbage', 'park', 'pirate', 'ski', 'state', 'whistle', 'palace', 'baseball', 'coal', 'queen', 'dominoes', 'photograph', 'computer', 'hockey', 'aircraft', 'hot', 'dog', 'salt', 'and', 'pepper', 'key', 'iPad', 'whisk', 'frog', 'lawnmower', 'mattress', 'pinwheel', 'cake', 'circus', 'battery', 'mailman']
-# print(random.randint())
-print(word_list[random.randint(0,len(word_list))])
+word = random.choice(word_list)
+print(word)
